f032a62c5f6eac00563da96ebd8831132aefbd2d
ItsToastCrumb <160540794+ItsToastCrumb@users.noreply.github.com>
2024-03-07 18:46:15 -0700

    completed js code to use image as dark/light theme toggle

---
+++ assets/js/logic.js
@@ -0,0 +1,20 @@
+//VARIABLE DECLARATIONS
+const themeToggleEl = document.querySelector('#themeToggle');
+const bodyEl = document.querySelector('body');
+const headerEl = document.querySelector('header')
+
+//Load current theme setting to window
+let mode = 'dark';
+
+//Light and Dark theme toggle
+//Event listener to toggle the page theme upon user click
+themeToggleEl.addEventListener('click', function(){
+    if (mode === 'dark'){
+        mode = 'light';
+        bodyEl.setAttribute('class', 'light');
+    } else {
+        mode = 'dark';
+        bodyEl.setAttribute('class', 'dark')
+    }
+    //Store current theme to local storage for future visits
+})
